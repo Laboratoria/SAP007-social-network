a694cbca718566fd67f8edf429678cf6d5500ecd
Layssa Aragão <oilayssaa@gmail.com>
2022-03-18 13:08:54 -0300

    clean code

--- src/config-firebase.js
+++ src/config-firebase.js
@@ -1,20 +1,15 @@
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
-// Your web app's Firebase configuration
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js';
+import { getDataBase } from 'https://www.gstatic.com/firebasejs/9.6.7/firebase-database.js';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDYuZfFIZPCxXmDcw9j3_dlkuyM-57OKWA",
-  authDomain: "girl-talk-app.firebaseapp.com",
-  projectId: "girl-talk-app",
-  storageBucket: "girl-talk-app.appspot.com",
-  messagingSenderId: "994363584873",
-  appId: "1:994363584873:web:6cf262fb179661b8b00df8",
+  apiKey: 'AIzaSyDYuZfFIZPCxXmDcw9j3_dlkuyM-57OKWA',
+  authDomain: 'girl-talk-app.firebaseapp.com',
+  projectId: 'girl-talk-app',
+  storageBucket: 'girl-talk-app.appspot.com',
+  messagingSenderId: '994363584873',
+  appId: '1:994363584873:web:6cf262fb179661b8b00df8',
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-
-
+const dataBase = getDataBase(app);
