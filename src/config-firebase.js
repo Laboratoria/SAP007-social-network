3c2faecd0ffd68142eb8d941738666464882cc40
Layssa Aragão <oilayssaa@gmail.com>
2022-03-18 10:01:46 -0300

    config firebase

---
+++ src/config-firebase.js
@@ -0,0 +1,17 @@
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAWnIJrGD7XNBEIDmno0Gda2LpElXmIU68",
+  authDomain: "grrrl-talk.firebaseapp.com",
+  projectId: "grrrl-talk",
+  storageBucket: "grrrl-talk.appspot.com",
+  messagingSenderId: "219826851050",
+  appId: "1:219826851050:web:6bdf78b8a8b72d0117db93",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
