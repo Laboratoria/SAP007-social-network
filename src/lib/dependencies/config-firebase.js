00505af7cbbcd822d0b2a408b2e8df9cb3104f34
Cássia Costa <cassia.silva93@hotmail.com>
2022-03-18 22:32:30 -0300

    Config firebase

--- src/lib/dependencies/config-firebase.js
+++ src/lib/dependencies/config-firebase.js
@@ -1,12 +1,13 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCn_iRDgagIUXjBSAQr5ea1vlvyITYaaTk',
-  authDomain: 'social-network-e4c45.firebaseapp.com',
-  projectId: 'social-network-e4c45',
-  storageBucket: 'social-network-e4c45.appspot.com',
-  messagingSenderId: '706546676635',
-  appId: '1:706546676635:web:e75dfdfd1cf5b6f906673e',
+  apiKey: "AIzaSyCHQm4GkaSIhilU-MdYk7iItd09BUzqFA0",
+  authDomain: "social-network-1df01.firebaseapp.com",
+  projectId: "social-network-1df01",
+  storageBucket: "social-network-1df01.appspot.com",
+  messagingSenderId: "794846527579",
+  appId: "1:794846527579:web:89c6446e30a6a972493f7e"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
