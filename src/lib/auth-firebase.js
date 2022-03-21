4a601c3ae332d5efdb0d8cc40b497d9b2b4664b6
lud-ic <mag.ludmila@gmail.com>
2022-03-21 17:21:03 -0300

    feat[auth-firebase]: create account

--- src/lib/auth-firebase.js
+++ src/lib/auth-firebase.js
@@ -6,18 +6,20 @@ import {
   signOut,
 } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-auth.js";
 
-const auth = getAuth(firebaseApp);
-createUserWithEmailAndPassword(auth, email, password)
+const auth = getAuth();
+
+export function userCreate(email, password) {
+  return createUserWithEmailAndPassword(auth, email, password)
   .then((userCredential) => {
-    // Signed in
     const user = userCredential.user;
-    // ...
+      return user;
   })
   .catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
-    // ..
+      return errorCode, errorMessage;
   });
+}
 
 // const auth = getAuth();
 signInWithEmailAndPassword(auth, email, password)
--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import "./lib/config-firebase.js";
-import { myFunction } from "./lib/index.js";
+import { userCreate, userLogout } from "./lib/auth-firebase.js";
+const buttonSubmit = document.getElementById("buttonSubmit");
+
+buttonSubmit.addEventListener("click", async (e) => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const tryingLogin = await userCreate(email, password);
+  console.log(tryingLogin);
+});
 
 myFunction();
