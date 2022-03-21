97ba45d9241491492eb4d5f1d40b8b54b5d00e63
Fe-Monteiro <fe.199nanda@gmail.com>
2022-03-21 10:05:09 -0300

    Instalação Firebase.

---
+++ src/lib/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAOhKqo7lJpdWp4hLImccHwhEKVTgG2jo4",
+  authDomain: "devdoubt---social-network.firebaseapp.com",
+  projectId: "devdoubt---social-network",
+  storageBucket: "devdoubt---social-network.appspot.com",
+  messagingSenderId: "1088547453612",
+  appId: "1:1088547453612:web:e3e36bc7cd272e7a0cfd09"
+};
+
+
+const app = initializeApp(firebaseConfig);
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 // Este es el punto de entrada de tu aplicacion
-
+import "./lib/firebase.js"
 import { myFunction } from './lib/index.js';
 
 myFunction();
