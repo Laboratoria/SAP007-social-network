a0270a4bf1e8a09115013922e44a03925c13f31e
Cássia Costa <cassia.silva93@hotmail.com>
2022-03-23 18:20:41 -0300

    Arquivo routes

--- src/main.js
+++ src/main.js
@@ -3,31 +3,3 @@
 // import { myFunction } from './lib/index.js';
 
 // myFunction();
-import login from './pages/login.js';
-// import feed from './pages/feed.js';
-import singUp from './pages/singup.js';
-
-const main = document.querySelector('#root');
-const init = () => {
-  window.addEventListener('hashchange', () => {
-    main.innerHTML = '';
-    switch (window.location.hash) {
-      case '':
-        main.appendChild(login());
-        break;
-      case '#feed':
-        main.appendChild(feed());
-        break;
-      case '#singUp':
-        main.appendChild(singUp());
-        break;
-      default:
-        main.appendChild(login()); //criar error
-    }
-  });
-};
-
-window.addEventListener('load', () => {
-  main.appendChild(login());
-  init();
-});
