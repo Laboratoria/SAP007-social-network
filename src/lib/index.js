a4f04d69589b81dac77f93bff2620773f87020ca
suelenescorcio <suelen919@gmail.com>
2022-03-23 18:17:47 -0300

    pastas e firebase

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,14 @@
-// aqui exportaras las funciones que necesites
-
-export const myFunction = () => {
-  // aqui tu codigo
-  console.log('Hola mundo!');
+const auth = getAuth();
+const criarUser = () => {
+  return createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
 };
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-// Este es el punto de entrada de tu aplicacion
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
 
-import { myFunction } from './lib/index.js';
 
-myFunction();
+const firebaseConfig = {
+  apiKey: "AIzaSyAZf-VOOI__art51kU5_YzbrS99bPR5NXw",
+  authDomain: "Rede-social-network.firebaseapp.com",
+  projectId: "Rede-social-network",
+  storageBucket: "Rede-social-network.appspot.com",
+  messagingSenderId: "81827701203",
+  appId: "1:81827701203:web:79cefd59735be7c86e0f26",
+};
+
+const app = initializeApp(firebaseConfig);
