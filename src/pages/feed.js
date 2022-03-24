f368efc947918444d7a0b275efcffcb6a6200289
dayanersilva <dayane.rodrigues_silva@hotmail.com>
2022-03-24 13:59:42 -0300

    alterações

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -5,12 +5,13 @@ export default () => {
   main.innerHTML = '';
   const feed = document.createElement('div');
   const container = `
-  <form clas="form-area" id="post-container">
+  <form class="form-area" id="post-container">
         <textarea type="text" class="post-area" placeholder=" Conte para nós sua experiência" name="mensagem"
           id="mensagem"></textarea>
-        <button type="submit" class="post-btn" id="btn-postar">postar</button>
+        <button type="submit" class="post-btn" id="btn-postar">Postar</button>
       </form>
       `;
 
   feed.innerHTML = container;
+  return feed
 }
