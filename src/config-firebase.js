2c3f09f8df25a58032f0019fc4df9d82340a6cb5
Erika Moreno <erika.kmoreno@gmail.com>
2022-03-25 15:04:12 -0300

    first commit

---
+++ src/config-firebase.js
@@ -0,0 +1,12 @@
+import  { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.9/firebase-app.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyDRDtG0KOp9htjlzbsm3Zrc1S65IOmLdzM",
+  authDomain: "go-trip-1874c.firebaseapp.com",
+  projectId: "go-trip-1874c",
+  storageBucket: "go-trip-1874c.appspot.com",
+  messagingSenderId: "312474392410",
+  appId: "1:312474392410:web:d20056044bd92331f1c563"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,4 @@
-// aqui exportaras las funciones que necesites
-
+// aqui exportaras las funciones que necesite
 export const myFunction = () => {
   // aqui tu codigo
   console.log('Hola mundo!');
