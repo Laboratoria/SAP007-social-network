38efd58733aace5f405be08c3321b6e5a28172be
BeaSCarvalho <beaproscarva@gmail.com>
2022-03-24 22:26:34 -0300

    com as funções de authentication

---
+++ src/configs/start-firebase.js
@@ -0,0 +1,16 @@
+//instalação inicial do firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBABEU0B6sje9XrJfZ0wq1H64qqxX7shXY",
+  authDomain: "eu-poesia---social-network.firebaseapp.com",
+  projectId: "eu-poesia---social-network",
+  storageBucket: "eu-poesia---social-network.appspot.com",
+  messagingSenderId: "237220348475",
+  appId: "1:237220348475:web:15fee5c697c2ba5bc7d21e"
+};
+
+const app = initializeApp(firebaseConfig);
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,17 +1,7 @@
-// Este es el punto de entrada de tu aplicacion
-
 import { myFunction } from './lib/index.js';
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js";
+import "./configs/start-firebase.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBABEU0B6sje9XrJfZ0wq1H64qqxX7shXY",
-  authDomain: "eu-poesia---social-network.firebaseapp.com",
-  projectId: "eu-poesia---social-network",
-  storageBucket: "eu-poesia---social-network.appspot.com",
-  messagingSenderId: "237220348475",
-  appId: "1:237220348475:web:15fee5c697c2ba5bc7d21e"
-};
-
-const app = initializeApp(firebaseConfig);
 myFunction();
 
+//a lista de rotas (window.location) pode ser criada aqui (main) ou em um arquivo separado de rotas
