a7d0196fb0f6e6eac6e733231cbc85fa09b53363
Fe-Monteiro <fe.199nanda@gmail.com>
2022-03-25 10:43:05 -0300

    Validações e-mail e senha.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,26 @@
-import home from "./Pages/home.js";
 import "./lib/firebase.js";
+import home from "./pages/home.js";
+import register from "./pages/register.js";
+//import timeline from "./pages/timeline.js";
+
 
 const main = document.querySelector("#root");
 
+const init = () => {
+    window.addEventListener("hashchange", () => {
+        main.innerHTML = "";
+        switch(window.location.hash){
+            case "#register":
+                main.appendChild(register());
+                break;
+            default:
+                main.appendChild(home());
+        }
+    })
+}
+
 window.addEventListener("load", () =>{
     main.appendChild(home());
+    init();
 })
 
