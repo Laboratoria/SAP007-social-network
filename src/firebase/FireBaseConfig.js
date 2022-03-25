91ebbca3af5b2867265dbff0dbbc2798e614c90f
Natalie I. S. Santos <natalie_ingrid@hotmail.com>
2022-03-25 20:21:49 -0300

    Feature login e senha

---
+++ src/firebase/FireBaseConfig.js
@@ -0,0 +1,23 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDDolPc8XVDjQDsR2H1ShETWYckqVfsFK0",
+    authDomain: "neweco-2022.firebaseapp.com",
+    projectId: "neweco-2022",
+    storageBucket: "neweco-2022.appspot.com",
+    messagingSenderId: "137738654234",
+    appId: "1:137738654234:web:fd315e0c36256e4a9b37a3",
+    measurementId: "G-6RLQY7GY25"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+
+
