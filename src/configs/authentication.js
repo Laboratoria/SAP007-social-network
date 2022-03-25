dbae22e787bfdf1f6ee5ef13fb3256e183b444cf
MariaLuizaSantana <costamalu123@outlook.com>
2022-03-25 17:05:53 -0300

    configurações do auth do  Firebase atualizadas

--- src/configs/authentication.js
+++ src/configs/authentication.js
@@ -10,35 +10,33 @@ const auth = getAuth();
 /*Login de novos usuários -
 É necessário criar uma função que será exportada para main.js que receberá o email e senha de lá
 createUserWithEmailAndPassword será o retorno.*/
-createUserWithEmailAndPassword(auth, email, password)
+export function newUser (email, password){
+  return createUserWithEmailAndPassword(auth, email, password)
   .then((userCredential) => {
-    // Signed in
     const user = userCredential.user;
-    // ...
   })
   .catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
-    // ..
 });
-  
+}
 
 //Login de usuários existentes - mesma coisa, signInWithEmailAndPassword é o retorno
-signInWithEmailAndPassword(auth, email, password)
+export function existingUser (email, password){
+  return signInWithEmailAndPassword(auth, email, password)
   .then((userCredential) => {
-    // Signed in
     const user = userCredential.user;
-    // ...
-  })
+    return user;
+    })
   .catch((error) => {
-    const errorCode = error.code;
     const errorMessage = error.message;
-});
-
-
+    return errorMessage;
+  });
+}
 
 //Definir um observador do estado de autenticação e coletar dados dos usuários
-onAuthStateChanged(auth, (user) => {
+export function dataCollector (email, password){
+return onAuthStateChanged(auth, (user) => {
   if (user) {
     // User is signed in, see docs for a list of available properties
     // https://firebase.google.com/docs/reference/js/firebase.User
@@ -49,4 +47,4 @@ onAuthStateChanged(auth, (user) => {
     // ...
   }
 });
-
+}
