1ba5ee10dd53657daece255e1562d6de64619d16
Erika Moreno <erika.kmoreno@gmail.com>
2022-03-28 18:41:44 -0300

    ajustes de nomes declarados

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,19 +1,16 @@
 export default () => {
   const container = document.createElement("div");
-
   const templateHome = `
   <p class="wellcome"> Bem vindo(a) viajante!! </p>
   <form action="#" id="sign-in-form" class="sign-in-form form">
-  <input type="email" placeholder="E-mail" id="email" />
+  <input type="email" placeholder="Email" id="email" />
   <input class= "inputs" type="password" placeholder="Senha" id="password"/>
   </form>
   <input class="btnEnter" type="submit" value="Entrar" id="entrar"/>
   <p class="enterByGoogle">Ou entre com o Google</p>
-  <p class="wellcome"> Ainda não tem cadastro ?</p>
-  <a href="#" id='sign-up-google'class="social-icon"> Registra-se!</a>
-
+  <p class="wellcome"> Ainda não tem cadastro?
+  <a href="#" id='sign-up-google'class="social-icon">Registre-se</a></p>
    `;
   container.innerHTML = templateHome;
-
   return container;
 };
