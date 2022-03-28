e6a551f0a3682aa3c6e0951a043648cde04a385a
Julia Benedicto <juliabenedictohfvcjd@gmail.com>
2022-03-28 13:34:27 -0300

    commit initial

---
+++ src/config-firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAGrWsT7kTZ2_UtPKCFzMleOBr3TCZ3dA8",
+  authDomain: "mirama-social-network.firebaseapp.com",
+  projectId: "mirama-social-network",
+  storageBucket: "mirama-social-network.appspot.com",
+  messagingSenderId: "887687261584",
+  appId: "1:887687261584:web:654afba21039e35c469715"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 // Este es el punto de entrada de tu aplicacion
-
+import "./config-firebase.js"
 import { myFunction } from './lib/index.js';
 
 myFunction();
