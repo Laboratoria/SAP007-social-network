f949f113dc2762bb41ba2efc3f0bcd8901e35382
moreirathais <thais_mjs@hotmail.com>
2022-03-28 09:20:53 -0300

    Alteração função validator

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,30 +1,33 @@
 export default () => {
-    const container = document.createElement("form");
-        container.setAttribute("class", "container");
+  const container = document.createElement("form");
+  container.setAttribute("class", "container");
 
-    const template = `
+  const template = `
         <input class= "input-email" placeholder= "e-mail" type= "email" required></input>
         <span class="error"></span>
         <input class= "input-password" placeholder= "senha" minlength= "6" type= "password" required></input>
-        <button class= "enter" type= "submit" onclick="return validator()">Entrar</button>
+        <button class= "enter" type= "submit">Entrar</button>
         <div class= "register"><a href="#register">Cadastre-se</a></div>
     `;
 
-    container.innerHTML = template;
+  container.innerHTML = template;
 
-    return container;
-}
+  function validator(e) {
+      e.preventDefault()
+      const email = container.querySelector(".input-email");
+    // const password = formuser.password.value;
+    const error = container.querySelector(".error");
 
-// function validator() {
-//     const email = document.querySelector(".input-email");
-//    // const password = formuser.password.value;
-//     const error = document.querySelector(".error");
+    if (email == "") {
+      error.innerHTML = "Preencha o campo e-mail";
+      email.focus();
+      return false;
+    }
+  }
 
-//     if(email == ""){
-//         error.innerHTML = "Preencha o campo e-mail";
-//         email.focus();
-//         return false;
-//     }
-// }
+  const enter = container.querySelector(".enter");
 
-// validator();
+  enter.addEventListener("click",validator)
+
+  return container;
+};
