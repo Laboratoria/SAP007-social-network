a81f66213cadfcac77a1acb027d31f5ba0e6542c
Cássia Costa <cassia.silva93@hotmail.com>
2022-03-28 22:22:32 -0300

    Estilização da page login / história de usuário 1

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,25 +1,35 @@
 export default () => {
   const areaLogin = document.createElement('div');
+  areaLogin.classList.add('login');
   areaLogin.innerHTML = `
   <div class="main-login">
+    <img src="../img/logo.png" alt="Logo Laboriam" class="logo">
     <h2>Login</h2>
     <form method="post">
       <input type="email" placeholder="E-mail">
-      <br>
       <input type="password" placeholder="Senha">
-      <br>
-      <button type="submit" id="btnEntrar">
+      <button type="submit" id="btnEntrar" class="btnEntrar">
         <a href="/#feed">Entrar</a>
       </button>
     </form>
-    <hr>
-    <button id="google">Entrar com o Google</button>
-    <button>Entrar com o Facebook</button>
-    <button>Entrar com o GitHub</button>
-    <hr>
-    <button>
-      <a href="/#singUp">Criar conta</a>
-    </button>
+    <p class="recoverPassword">Esqueceu a senha?</p>
+    <div class="buttons">
+      <p>Ou entre com:</p>
+      <button class="google" id="google">
+        <img src="../img/icon-google.png" alt="" class="iconGoogle">
+      </button>
+      <button class="facebook">
+        <img src="../img/icon-facebook.png" alt="" class="iconFacebook">
+      </button>
+      <button class="github">
+        <img src="../img/icon-github.png" alt="" class="iconGithub">
+      </button>
+    </div>
+    <p class="criarConta">Não tem uma conta? <a href="/#singUp">Criar conta</a></p>
+    <div class="devs">
+      <p><img src="../img/copyright.png" alt="" class="copyright"> Desenvolvido por:</p>
+      <p>Cássia Costa, Dayane Rodrigues e Viviane Soares</p>
+    </div>
   </div>
 `;
   return areaLogin;
