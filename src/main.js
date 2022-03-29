857d0fd9425f2c7b7d5d2d94ac0e7f2585edd9d9
adrianatwatanabe <adriana.t.watanabe@gmail.com>
2022-03-29 11:04:26 -0300

    Autenticação

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { myFunction } from "./lib/index.js";
+// import { myFunction } from "./lib/index.js";
 import "./js/config-firebase.js";
 
-const app = initializeApp(firebaseConfig);
+
 
 myFunction();
