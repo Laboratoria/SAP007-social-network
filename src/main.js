d351d043adfe2df21086ea96e7981e65b2fc3694
loulima <louizalima13@gmail.com>
2022-03-28 21:38:58 -0300

    pag login

--- src/main.js
+++ src/main.js
@@ -2,10 +2,7 @@ import "./config.firebase.js"
 
 import login from "../pages/login/index.js"
 
-
-
 const main = document.querySelector("#root");
-
 
 window.addEventListener("load",() => {
     console.log('load')
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,23 +1,24 @@
-export default () =>{
-    const container = document.createElement('div');
-    const template =`
+export default () => {
+  const container = document.createElement("div");
+  const template = `
     <section class="container-main-login">
-      <div class="login-container">
-      <form class="">
-    <label for="" class="">Email</label>
-    <input class="" type="email" name="" id="">
-    <br>
-    <label for="" class="">Senha</label>
-    <input class="" type="password" name="" id="">
-    <br>
-    <button class="" type="submit" id="">Entrar</button>
+  <div class="login-container">
+    <form class="">
+      <label for="" class="">Email</label>
+      <input class="text-input" type="email" name="" id="">
+      <br>
+      <label for="" class="">Senha</label>
+      <input class="text-input" type="password" name="" id="">
+      <br>
+      <button class="btn-login" type="submit" id="">Entrar</button>
+      <p>OU</p>
+      <button class="btn-login-google" type="submit" id="">Entrar com google</button>
+      <button class="btn-register" type="submit" id="">Não possui uma conta?cadastra-se</button>
     </form>
-    </div>
-    </section>
+  </div>
+</section>
     `;
-    container.innerHTML=template;
-    console.log('correu load')
-    return container;
-    
-  }
-  
+  container.innerHTML = template;
+  console.log("correu load");
+  return container;
+};
