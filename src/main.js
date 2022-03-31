d0fcb569bcd61edf9e7cba51972a4cf17a1e6646
Daiane Oltramari <daianeoltramari@gmail.com>
2022-03-31 19:23:21 -0300

    alterações no arquivo main

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.9/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.9/firebase-auth.js";
 
 
 const firebaseConfig = {
   apiKey: "AIzaSyAZf-VOOI__art51kU5_YzbrS99bPR5NXw",
-  authDomain: "Rede-social-network.firebaseapp.com",
-  projectId: "Rede-social-network",
-  storageBucket: "Rede-social-network.appspot.com",
+  authDomain: "young-social-network.firebaseapp.com",
+  databaseURL: "https://young-social-network-default-rtdb.firebaseio.com",
+  projectId: "young-social-network",
+  storageBucket: "young-social-network.appspot.com",
   messagingSenderId: "81827701203",
-  appId: "1:81827701203:web:79cefd59735be7c86e0f26",
+  appId: "1:81827701203:web:79cefd59735be7c86e0f26"
 };
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
