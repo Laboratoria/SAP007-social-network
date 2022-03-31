043ad9cc9263b14ecc575cc5c6a0faacafc31a2f
moreirathais <thais_mjs@hotmail.com>
2022-03-31 07:55:22 -0300

    início de utilização do observador

--- src/lib/authentication.js
+++ src/lib/authentication.js
@@ -3,14 +3,12 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   GoogleAuthProvider,
-  signInWithPopup
-  // signInWithRedirect,
-  // getRedirectResult
+  signInWithPopup,
+  // onAuthStateChanged 
 } from "https://www.gstatic.com/firebasejs/9.6.9/firebase-auth.js";
 
 const provider = new GoogleAuthProvider();
 const auth = getAuth();
-// signInWithRedirect(auth, provider);
 
 export function userCreate(email, password) {
   return createUserWithEmailAndPassword(auth, email, password).then(
@@ -30,15 +28,13 @@ export function userLogin(email, password) {
   );
 }
 
-
-
 export const googleLogin = () => {
    return signInWithPopup(auth, provider)
   .then((result) => {
     // This gives you a Google Access Token. You can use it to access the Google API.
     const credential = GoogleAuthProvider.credentialFromResult(result);
     const token = credential.accessToken;
-    // The signed-in user info.
+    // The signed-in user info.30
     const user = result.user;
     // ...
   }).catch((error) => {
@@ -53,22 +49,7 @@ export const googleLogin = () => {
   });
 
 }
-//   getRedirectResult(auth)
-//   .then((result) => {
-//     // This gives you a Google Access Token. You can use it to access Google APIs.
-//     const credential = GoogleAuthProvider.credentialFromResult(result);
-//     const token = credential.accessToken;
 
-//     // The signed-in user info.
-//     const user = result.user;
-//   }).catch((error) => {
-//     // Handle Errors here.
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-//     // The email of the user's account used.
-//     const email = error.email;
-//     // The AuthCredential type that was used.
-//     const credential = GoogleAuthProvider.credentialFromError(error);
-//     // ...
-//   });
-// }
+// export const onAuthStateChanged = (auth, user)=> {
+//   return onAuthStateChanged(auth, user)
+// };
