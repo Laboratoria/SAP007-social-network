fff8c9047c900cee271bf0654e796a9b6fd3675f
Marione-Pereira <mari.tainara@gmail.com>
2022-04-01 20:08:05 -0300

    teste rotas

--- src/login/LoginFunctions.js
+++ src/login/LoginFunctions.js
@@ -1,5 +1,77 @@
-// eslint-disable-next-line
+import '../firebase/FireBaseConfig.js';
 import {
+  signinPassword,signinWithGoogle
+} from '../firebase/authentication.js'
+
+
+  export default() => {
+  const containerLogin = document.creatElement("div");
+  const templateLogin =`
+    <section id="login" class="box-login"
+    <h3>login</h3>
+  <section class="input-login">
+    <input type="email" name="email" id="email-input" placeholder="Insera e-mail" required />
+    <section class="input-login">
+      <input type="password" name="password" id="password-input" placeholder="Insera uma senha"  requerid/>
+    </section>
+    <section id="recover-password"></section>
+    <a href=""> Esqueceu a sua senha?</a>
+  </section>
+  <button type="submit" id="btn-login">Entrar</button>
+  <p>Ou</p>
+  <button type="submit" id="btn-google">Entrar com o Google</button>
+  <hr />
+</section>
+<p>Não tem conta?</p>
+<a href="btn-register" href="/#"> Cadastre-se</a>
+  `;
+  containerLogin.innerHTML = templateLogin;
+
+  
+   export default() => {
+    const containerLogin = document.creatElement("form");
+    const templateLogin =`
+      <section id="login" class="box-login"
+      <h3>login</h3>
+    <section class="input-login">
+      <input type="email" name="email" id="email-input" placeholder="Insera e-mail" required />
+      <section class="input-login">
+        <input type="password" name="password" id="password-input" placeholder="Insera uma senha"  requerid/>
+      </section>
+      <section id="recover-password"></section>
+      <a href=""> Esqueceu a sua senha?</a>
+    </section>
+    <button type="submit" id="btn-login">Entrar</button>
+    <p>Ou</p>
+    <button type="submit" id="btn-google">Entrar com o Google</button>
+    <hr />
+  </section>
+  <p>Não tem conta?</p>
+  <a href="btn-register" href="/#"> Cadastre-se</a>
+    `;
+    containerLogin.innerHTML = templateLogin;
+  
+    const getEmail = document.querySelector('#email-input');
+    const getPassword = document.querySelector('#password-input');
+   const form = containerLogin.getElementById("")
+    containerLogin.addEventListener("submit", (e) => {
+          e.preventDefault();
+        signinPassword(getEmail.value, getPassword.value)
+        .then((userCredential) => {
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage, errorCode);
+        
+  
+    };
+  
+
+
+
+
+/*import {
   getAuth,
   signInWithEmailAndPassword,
 } from 'https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js';
--- src/routes/main.js
+++ src/routes/main.js
@@ -1,5 +1,47 @@
+import '../firebase/FireBaseConfig.js';
+import {about} from '../about/about.js'
 
-import HtlmHome from '../Home/Home.js'
+const main = document.querySelector('#root');
+const renderizar = () => {
+  window.addEventListener('hashchange', () => {
+    main.innerHTML = "";
+    switch (window.location.hash) {
+      case "":
+        main.appendChild(home());
+        break;
+      case "#about":
+        main.appendChild(about());
+        break;
+      case "#signUp":
+        main.appendChild(register());
+        break;
+      case "#feed":
+        main.appendChild(feed());
+        break;
+      case "#login":
+        main.appendChild(login());
+        break;
+      default:
+        main.appendChild(home());
+        break;
+    }
+  });
+};
+
+window.addEventListener("load", () => {
+  main.appendChild("home"());
+  renderizar();
+});
+
+
+
+
+
+
+
+
+
+/*import HtlmHome from '../Home/Home.js'
 import HtlmLogin from '../Home/Home.js'
 const routes = {
   404: '<h1>Pagina não encontrada</h1>',
@@ -14,7 +56,7 @@ const handleLocation = () => {
 
   /* O link de home passa uma # apenas e quando usamos window.location.hash, ele pega o que vem a # e o que vem apos, caso tenha apenas a # ela vem uma string vazia, essa condição é para caso o window.location.hash nos traga uma string vazia, ela ser alterada pra '#', para conseguir buscar dentro do objeto routes */
 
-  if (path === '') {
+ /* if (path === '') {
     path = '#';
   }
 
