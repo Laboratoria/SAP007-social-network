e128f4dc8cf2578910841fededbae348082fcc08
WayraArendartchukCastro <wayraac@gmail.com>
2022-04-12 16:52:24 -0300

    correção aspas duplas

--- src/lib/config-firebase.js
+++ src/lib/config-firebase.js
@@ -1,12 +1,14 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js';
+import {
+  initializeApp
+} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA67hbBU9XpJYva3s_94wO5KasFRKw7iV8',
-  authDomain: 'leitoras.firebaseapp.com',
-  projectId: 'leitoras',
-  storageBucket: 'leitoras.appspot.com',
-  messagingSenderId: '105182267346',
-  appId: '1:105182267346:web:a9e72c8c1a031df4b42a0b',
+  apiKey: "AIzaSyA67hbBU9XpJYva3s_94wO5KasFRKw7iV8",
+  authDomain: "leitoras.firebaseapp.com",
+  projectId: "leitoras",
+  storageBucket: "leitoras.appspot.com",
+  messagingSenderId: "105182267346",
+  appId: "1:105182267346:web:a9e72c8c1a031df4b42a0b",
 };
 
 const app = initializeApp(firebaseConfig);
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 // Este es el punto de entrada de tu aplicacion
-import "../lib/config-firebase.js" 
-import { myFunction } from './lib/index.js';
+import "../lib/config-firebase.js"
+import {
+  myFunction
+} from "./lib/index.js";
 
 
 
