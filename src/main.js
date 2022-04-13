fcc97e8ecef6f479515e40fdd5d27bf90f384dfd
Jessika Mirella <jessikamirellabraga@gmail.com>
2022-04-13 13:25:27 -0300

    pagina de login ainda quebrada

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 // Este es el punto de entrada de tu aplicacion
-import "../lib/config-firebase.js"
-import { myFunction } from './lib/index.js';
-import {
-  myFunction
-} from "./lib/index.js";
+//import "../lib/config-firebase.js"
+//import { myFunction } from './lib/index.js';
+import login from "./pages/login"
 
+const main = document.querySelector("#root")
 
+window.addEventListener("load", () =>{
+  main.appendChild(login());
+
+}
+)
 
 
 myFunction();
