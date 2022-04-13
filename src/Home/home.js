62e46a9e075cb5fa9340477c88064633b2e8bab4
Marione-Pereira <mari.tainara@gmail.com>
2022-04-13 00:08:08 -0300

    adiconando CSS da home

--- src/Home/home.js
+++ src/Home/home.js
@@ -3,14 +3,14 @@ import '../firebase/firebaseconfig.js';
 export default () => {
   const containerHome = document.createElement('div');
   const templateHome = `
-  
   <h1 class='home-title'>Bem-Vindo ao Eco Work Planet</h1>
+  <div class='home-main'>
      <video src=""></video><br>
     <a href="#login" class='link-pages'>Login</a><br>
     <a href="#register" class='link-pages'>Cadastro</a><br>
     <a href="#about" class='link-pages'>Sobre</a><br>
 
-
+    </div>
  
   `;
   containerHome.innerHTML = templateHome;
