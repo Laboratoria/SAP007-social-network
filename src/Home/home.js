f5ac73354b4b8276baa08d80f8b1d4ee2b8b2ec5
Marione-Pereira <mari.tainara@gmail.com>
2022-04-13 11:27:34 -0300

    ajustando css mobile

--- src/Home/home.js
+++ src/Home/home.js
@@ -5,7 +5,7 @@ export default () => {
   const templateHome = `
   <h1 class='home-title'>Bem-Vindo ao Eco Work Planet</h1>
   <div class='home-main'>
-     <video src=""></video><br>
+     <video src="img/teste.mkv"></video><br>
     <a href="#login" class='link-pages'>Login</a><br>
     <a href="#register" class='link-pages'>Cadastro</a><br>
     <a href="#about" class='link-pages'>Sobre</a><br>
