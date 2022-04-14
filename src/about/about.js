f86829c81e9bc1b07105a2775a6d1e3bfbce7239
Natalie I. S. Santos <natalie_ingrid@hotmail.com>
2022-04-14 16:36:40 -0300

    primeiras estilizações pagina sobre

--- src/about/about.js
+++ src/about/about.js
@@ -2,20 +2,20 @@ export default () => {
   const container = document.createElement('div');
 
   const template = `
-    <h2>Sobre a plataforma:</h2>
     <div class="text-about">
-    <p>Esta plataforma foi criada em prol do meio ambiente e ecossistema. Este é um espaço para melhorar a visibilidade e alcance das ONGs de proteção ao meio ambiente espalhadas por todo o território brasileiro. Também é um ambiente onde as empresas eco-friendly e outras com iniciativas de preservação ao meio ambiente podem compartilhar e divulgar seus produtos a uma comunidade composta exclusivamente por adotantes do veganismo/vegetarianismo, estilo de vida sustentável e simpatizantes da causa.</p>
-    <h3>Sobre as idealizadoras:</h3>
-    <p><img src="https://avatars.githubusercontent.com/u/92555432?v=4.png" width="60"/></p>
-    <a href="https://github.com/KarinaMel0" target="_blank"> <p>🍀 Karina Mel Silva - 19 anos - Web Developer</p><a>
+    <p class="text-about">Sobre a plataforma:</p>
+    <p>Esta plataforma foi criada em prol do meio ambiente e ecossistema. Este é um espaço para melhorar a visibilidade e alcance das ONGs de proteção ao meio ambiente espalhadas por todo o território brasileiro. Também é um ambiente criado para que as empresas eco-friendly e outras com iniciativas de preservação ao meio ambiente possam compartilhar e divulgar seus produtos e ideias a uma comunidade composta exclusivamente por amantes da natureza, adotantes do veganismo/vegetarianismo, estilo de vida sustentável e simpatizantes da causa.</p>
+    <h3 class="text-about">Sobre as idealizadoras:</h3>
+    <picture><img class="developers-img" src="https://avatars.githubusercontent.com/u/92555432?v=4.png" width="60"/></picture>
+    <a href="https://github.com/KarinaMel0" target="_blank"> <p class="name" style="float: left">🍀 Karina Mel Silva - 19 anos - Web Developer</p><a>
    
 
-    <p><img src="https://avatars.githubusercontent.com/u/97197240?v=4.png" width="60"/></p>
-    <a href="https://github.com/natalieiss" target="_blank"><p>🍀 Natalie Silva - 34 anos - Web Developer</p><a>
+    <picture><img class="developers-img" src="https://avatars.githubusercontent.com/u/97197240?v=4.png" width="60"/></picture>
+    <a href="https://github.com/natalieiss" target="_blank"><p class="name">🍀 Natalie Silva - 34 anos - Web Developer</p><a>
 
     
-    <p><img src="https://avatars.githubusercontent.com/u/91857912?v=4.png" width="60"/></p>
-    <a href="https://github.com/Marione-Tainara" target="_blank"><p>🍀 Marione Pereira - 27 anos - Web Developer</p><a>
+    <picture><img class="developers-img" src="https://avatars.githubusercontent.com/u/91857912?v=4.png" width="60"/></picture>
+    <a href="https://github.com/Marione-Tainara" target="_blank"><p class="name">🍀 Marione Pereira - 27 anos - Web Developer</p><a>
     
 
     <a href ='#home' class ="return-home">Voltar a página home</a>
