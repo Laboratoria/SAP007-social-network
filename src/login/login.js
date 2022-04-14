b12ec1e3af7c6a39718132ac33ed8e11aded3c2c
Marione-Pereira <mari.tainara@gmail.com>
2022-04-13 23:02:20 -0300

    adicionando css page login

--- src/login/login.js
+++ src/login/login.js
@@ -3,20 +3,21 @@ import { signinPassword, googleLogin } from '../firebase/authentication.js';
 
 export const login = () => {
   const containerLogin = document.createElement('div');
-  containerLogin.setAttribute('class', 'container');
+  containerLogin.setAttribute('class', 'container-login');
   const templateLogin = `
   <form class="input-login">
   <p>Login</p>
-  <input type="email" name="email" class="email-input" placeholder="Insera e-mail" required /><br>
-  <input type="password" name="password" class="password-input" placeholder="Insera uma senha" requerid /><br>
+  <input type="email" name="email" class="email-input input" placeholder="Insera e-mail" required /><br>
+  <input type="password" name="password" class="password-input input" placeholder="Insera uma senha" requerid /><br>
   <a href=""> Esqueceu a sua senha?</a><br>
-  <br><button type="submit">Entrar</button><br>
+  <br><button  class="btn" type="submit">Entrar</button><br>
 
   <button class="btn-google"><img src="img/google.png" alt="botão Google">Entrar com o Google
   </button><br>
+  <p>Não tem conta? <a href="#register"> Cadastre-se</a></p>
 </form>
 </section>
-  <p>Não tem conta? <a href="#register"> Cadastre-se</a></p>
+
 
   
 
