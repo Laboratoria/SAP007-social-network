9e652e8bfa0973d9c35515606b95bababfd4f674
Marione-Pereira <mari.tainara@gmail.com>
2022-04-13 22:19:41 -0300

    tamanho medios quase pronto

--- src/Home/home.js
+++ src/Home/home.js
@@ -5,11 +5,10 @@ export default () => {
   const templateHome = `
   <h1 class='home-title'>Bem-Vindo ao Eco Work Planet</h1>
   <div class='home-main'>
-     <video src="img/teste.mkv"></video><br>
+     <video src="img/teste.mkv"></video>
     <a href="#login" class='link-pages'>Login</a><br>
     <a href="#register" class='link-pages'>Cadastro</a><br>
     <a href="#about" class='link-pages'>Sobre</a><br>
-
     </div>
  
   `;
