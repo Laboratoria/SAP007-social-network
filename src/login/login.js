4f354f701eaaf0e2da4fe1529dad2258f263742f
Marione-Pereira <mari.tainara@gmail.com>
2022-04-14 21:55:50 -0300

    ajustando CSS mobile login

--- src/login/login.js
+++ src/login/login.js
@@ -3,24 +3,20 @@ import { signinPassword, googleLogin } from '../firebase/authentication.js';
 
 export const login = () => {
   const containerLogin = document.createElement('div');
-  containerLogin.setAttribute('class', 'container-login');
+  containerLogin.setAttribute('class', 'container');
   const templateLogin = `
-  <form class="form-login">
-  <p class='paragraph'>Login</p>
-  <input type="email" name="email" class="email-input input-users" placeholder="Insera e-mail" required /><br>
-  <input type="password" name="password" class="password-input input-users" placeholder="Insera uma senha" requerid /><br>
+  <form class="input-login">
+  <p>Login</p>
+  <input type="email" name="email" class="email-input" placeholder="Insera e-mail" required /><br>
+  <input type="password" name="password" class="password-input" placeholder="Insera uma senha" requerid /><br>
   <a href=""> Esqueceu a sua senha?</a><br>
-  <br><button  class="btn-submit" type="submit">Entrar</button><br>
-
-  <button class="btn-google"><img src="img/google.png" alt="botão Google" class="logo-google">Entrar com o Google
+  <br><button type="submit">Entrar</button><br>
+  <button class="btn-google"><img src="img/google.png" alt="botão Google">Entrar com o Google
   </button><br>
-  <p>Não tem conta? <a href="#register"> Cadastre-se</a></p>
 </form>
 </section>
-
-
+  <p>Não tem conta? <a href="#register"> Cadastre-se</a></p>
   
-
   `;
   containerLogin.innerHTML = templateLogin;
   const email = containerLogin.querySelector('.email-input');
