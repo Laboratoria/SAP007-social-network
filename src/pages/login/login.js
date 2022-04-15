0cc6fff505599023031260a42d0ef64fc5fcfd03
AnnaIsah <annaisabellef@gmail.com>
2022-04-15 15:32:43 -0300

    login permanence state

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -4,10 +4,9 @@
 import { logar, logarGmail } from './authentication.js';
 
 export const pageLogin = () => {
-
-    const login = document.createElement('div');
-    login.setAttribute('class', 'box-form-login');
-    login.innerHTML = `    
+  const login = document.createElement('div');
+  login.setAttribute('class', 'box-form-login');
+  login.innerHTML = `    
     
       <figure class="box-slogan-page-login">
         <img src="./img/kfandom.svg" alt="Logotype" class="logo-icon-page-login">
@@ -16,16 +15,15 @@ export const pageLogin = () => {
           <input type="email" placeholder="seu@email.com" class="login-area" id="email-area" name="email-area" requered>
           <input type="password" placeholder="Senha" class="login-area" id="password-area" name="password-area" requered>
           <button class="btn-sign-in btn-area" id="btn-sign-in" >Entrar</button>
-          <p class="error" id = "user-error"></p>
+          
         </form>
+      <p class="error" id="user-error"></p> 
       <p class="error" id = "user-error-gmail"></p>
       <p class="text-center" >- ou -</p>
       <button class="btn-google text-center" id="btn-google"><img src="./img/G.svg" alt="btn-google" class="img-btn-google">Sign in with Google</button>
       <p class="text-create-login text-center">
         Ainda não tem conta? <a href="#createLogin" id="click-register">Cadastre-se</a>
-      </p>
-
- 
+      </p> 
   `;
 
   login.querySelector('#btn-sign-in').addEventListener('click', (e) => {
@@ -53,52 +51,38 @@ export const pageLogin = () => {
           case 'auth/internal-error':
             userError.innerHTML = '*Campos obrigatórios';
             userError.style.display = 'block';
+            break;
+          case 'auth/wrong-password':
+            userError.innerHTML = '*Email ou senhas estão errados';
+            userError.style.display = 'block';
+            break;
+          case 'auth/user-not-found':
+            userError.innerHTML = '*Usuário não cadastrado, registre-se!';
+            userError.style.display = 'block';
+            break;
 
-        } else if (inputEmail && inputPassword && invalidFormat) {
-            logar(inputEmail, inputPassword).then(() => {
-                window.location.hash = '#feed';
-            }).catch((error) => {
-                switch (error.code) {
-                    case 'auth/invalid-email':
-                        userError.innerHTML = '*Campos obrigatórios';
-                        userError.style.display = 'block';
-                        break;
-                    case 'auth/internal-error':
-                        userError.innerHTML = '*Campos obrigatórios';
-                        userError.style.display = 'block';
-                        break;
-                    case 'auth/wrong-password':
-                        userError.innerHTML = '*Email ou senhas estão errados';
-                        userError.style.display = 'block';
-                        break;
-                    case 'auth/user-not-found':
-                        userError.innerHTML = '*Usuário não cadastrado, registre-se!';
-                        userError.style.display = 'block';
-                        break;
-
-                    default:
-                }
-            });
+          default:
         }
+      });
+    }
+  });
 
+  login.querySelector('#btn-google').addEventListener('click', (e) => {
+    e.preventDefault();
+    const userErrorGmail = login.querySelector('#user-error-gmail');
+    logarGmail().then(() => {
+      window.location.hash = '#feed';
+    }).catch((error) => {
+      switch (error.code) {
+        case 'auth/user-disabled':
+          userErrorGmail.innerHTML = '*Não foi possivel logar com sua conta Google, por favor verifique seu login';
+
+          break;
+
+        default:
+      }
     });
-
-    login.querySelector('#btn-google').addEventListener('click', (e) => {
-        e.preventDefault();
-        const userErrorGmail = login.querySelector('#user-error-gmail');
-        logarGmail().then(() => {
-            window.location.hash = '#feed';
-        }).catch((error) => {
-            switch (error.code) {
-                case 'auth/user-disabled':
-                    userErrorGmail.innerHTML = '*Não foi possivel logar com sua conta Google, por favor verifique seu login';
-
-                    break;
-
-                default:
-            }
-        });
-    });
+  });
 
   return login;
 };
