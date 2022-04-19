7609b1039013af5f645205e52879391456c5fb14
Cássia Costa <cassia.silva93@hotmail.com>
2022-04-19 18:50:58 -0300

    Remoção dos comentários

--- src/services/firestore.js
+++ src/services/firestore.js
@@ -1,20 +1,12 @@
 import { collection, getDocs, addDoc, setDoc, doc } from 'https://www.gstatic.com/firebasejs/9.6.9/firebase-firestore.js';
 import { db } from '../dependencies/config-firebase.js';
 
-// Adicionar documento na função de post
 export function enviarPost(mensagemPost) {
   addDoc(collection(db, 'posts'), {
     novoTeste: mensagemPost
   });
 }
 
-// Adicionar usuários na collection
-// addDoc(collection(db, 'user'), {
-//   name: "Quarto teste",
-//   email: "cassia@hotmail.com"
-// });
-
-// Função de buscar os posts
 export async function getDoBanco() {
   const collectionPost = collection(db, 'posts');
   const postSnapshot = await getDocs(collectionPost);
