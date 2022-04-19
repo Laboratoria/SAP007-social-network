51b19c09643e6d5610d6e9d0c6f458c67ffcc78d
Camila Aguiar <camilaguiar73@gmail.com>
2022-04-18 22:12:31 -0300

    ajustando detalhes

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -6,26 +6,26 @@ export default () => {
   const container = document.createElement('div');
   container.classList.add('containerA');
   const templateHome = `  
-  <div class="center">
-  <div class="banner"> 
-  </div>
-  <div class= "main-container">
-       <div class="container-fluid">
-      <p class="welcome"> Bem vindo(a) viajante! </p>
-      <form action="#" id="sign-in-form" class="sign-in-form">
-        <input class= "inputs" type="email" placeholder="E-mail" id="email" />
-        <input class= "inputs" type="password" placeholder="Senha" id="password"/>
-        <span class="error" id ="error"></span>
-        <button class="btnEnter" type="submit" value="Entrar" id="btnEnter">Entrar</button>
-      </form>
-      <p class="enterByGoogle">Ou entre com o Google</p>
-     <a href="/" class="google-link">
-       <img  class="logoGoogle" src="./img/google.png" alt="logo Google">
-     </a>
-       <p class="welcome"> Ainda não tem cadastro?</p>
-       <a href="#register" id='sign-up-google'class="link">Registre-se</a></p>  
-     </div>  
-  </div>
+    <div class="center">
+      <div class="banner"> </div>
+      <div class= "main-container">
+        <div class="container-fluid">
+          <p class="welcome"> Bem vindo(a) viajante! </p>
+          <form action="#" id="sign-in-form" class="sign-in-form">
+            <input class= "inputs" type="email" placeholder="E-mail" id="email" />
+            <input class= "inputs" type="password" placeholder="Senha" id="password"/>
+            <span class="error" id ="error"></span>
+            <button class="btnEnter" type="submit" value="Entrar" id="btnEnter">Entrar</button>
+          </form>
+          <p class="enterByGoogle">Ou entre com o Google</p>
+          <a href="/" class="google-link">
+            <img  class="logoGoogle" src="./img/google.png" alt="logo Google">
+          </a>
+          <p class="welcome"> Ainda não tem cadastro?</p>
+          <a href="#register" id='sign-up-google'class="link">Registre-se</a></p>  
+        </div>  
+      </div>
+    </div>
   `;
   container.innerHTML = templateHome;
 
@@ -46,7 +46,7 @@ export default () => {
       .catch((response) => {
         if (response.code == 'auth/invalid-email') {
           message.innerHTML = 'Digite um e-mail válido';
-        } else if (response.code == 'auth/internal-error') {
+        } if (response.code == 'auth/internal-error') {
           message.innerHTML = 'Senha inválida';
         }
       })
