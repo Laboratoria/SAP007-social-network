3f00a60938ac32d2857c1534cf702ba5d14603e8
ThamaraBortolin <thamaralbortolin@gmail.com>
2022-04-21 15:50:46 -0300

    firebase

--- src/pages/firebase/config-firebase.js
+++ src/pages/firebase/config-firebase.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
-import "../"
 
 const firebaseConfig = {
   apiKey: "AIzaSyCemQ-w-0HGmEOPIReFIFOhiwToyyzsd4A",
