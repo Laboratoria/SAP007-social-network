016a94bfe3192a2acdade804830ec362accf299a
AnnaIsah <annaisabellef@gmail.com>
2022-04-24 17:59:18 -0300

    componentization of error handling and page styling

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,6 +1,6 @@
 export const pageAbout = () => {
   const about = document.createElement('div');
-  about.setAttribute('class', 'box-form-login');
+  about.setAttribute('class', 'box-about');
   about.innerHTML = `    
     
       <figure class="box-slogan-page-login">
@@ -11,38 +11,30 @@ export const pageAbout = () => {
         <h1 class="tittle-about">Lorem ipsum dolor sit amet</h1>      
         <p class="text-about">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </section>
-
-      <section class="devs-about">
-        <h2 class="tittle-devs-about">Desenvolvido por:</h2>      
-        <picture>
-          <img src="./img/Anna.jpg" alt="picture-dev-anna" class="picture-dev">
-            <a href="#open-modal-anna"> Anna Ferraz</a>
-            
-              <section class="modal-devs" id="open-modal-anna">
-                <a href="#close-modal-anna" class="close">x</a>
-                <a href="https://github.com/AnnaIsah" target="_blank">GitHub</a>
-                <a href="https://www.linkedin.com/in/anna-ferraz/" target="_blank">LinkeIn</a>
-              </section>  
+      <h2 class="tittle-devs-about">Desenvolvido por:</h2>
+      
+      <section class="about-devs">       
+        <div class="div-picture-devs">    
+            <img src="./img/Anna.jpg" alt="picture-dev-anna" class="picture-devs">                
+              <h3> Anna Ferraz</h3>                   
+                <p><a href="https://github.com/AnnaIsah" target="_blank">GitHub</a>
+                <a href="https://www.linkedin.com/in/anna-ferraz/" target="_blank">LinkeIn</a></p>          
+        </div>              
           
-          <img src="./img/Monica.jpg" alt="picture-dev-anna" class="picture-dev">
-            <a href="#open-modal-monica"> Mônica Guimarães</a>
-            
-              <section class="modal-devs" id="open-modal-monica">
-                <a href="#close-modal-monica">x</a>
-                <a href="https://github.com/MonicaGuimaraes" target="_blank">GitHub</a>
-                <a href="https://www.linkedin.com/in/monica-peixoto-guimaraes-v/" target="_blank">LinkeIn</a>
-              </section>  
+        <div class="div-picture-devs">
+            <img src="./img/Monica.jpg" alt="picture-dev-anna" class="picture-devs">         
+              <h3> Mônica Guimarães</h3>         
+                <p><a href="https://github.com/MonicaGuimaraes" target="_blank">GitHub</a>
+                <a href="https://www.linkedin.com/in/monica-peixoto-guimaraes-v/" target="_blank">LinkeIn</a><p>
+        </div>               
           
-          <img src="./img/Taize.jpg" alt="picture-dev-anna" class="picture-dev">
-            <a href="#open-modal-taize"> Taize dos Santos</a>
-            
-              <section class="modal-devs" id="open-modal-taize">
-                <a href="#close-modal-taize">x</a>
-                <a href="https://github.com/taizesantos" target="_blank">GitHub</a>
-                <a href="https://www.linkedin.com/in/taizeborges/" target="_blank">LinkeIn</a>
-              </section>  
-        </picture>
+        <div class="div-picture-devs">            
+            <img src="./img/Taize.jpg" alt="picture-dev-anna" class="picture-devs">         
+              <h3> Taize dos Santos</h3>        
+                <p><a href="https://github.com/taizesantos" target="_blank">GitHub</a>
+                <a href="https://www.linkedin.com/in/taizeborges/" target="_blank">LinkeIn</a></p> 
+        </div>                    
       </section>  
-  `;
+     `;
   return about;
 };
---
+++ src/pages/login/errorHandling.js
@@ -0,0 +1,28 @@
+export const errorHandlingGeral = (error) => {
+  let valorRetorno;
+
+  switch (error.code) {
+    case 'auth/invalid-email':
+      valorRetorno = 'Campos obrigatórios';
+
+      break;
+    case 'auth/internal-error':
+      valorRetorno = 'Campos obrigatórios';
+
+      break;
+    case 'auth/wrong-password':
+      valorRetorno = 'Email ou senhas estão errados';
+
+      break;
+    case 'auth/user-not-found':
+      valorRetorno = 'Usuário não cadastrado, registre-se!';
+
+      break;
+    case 'auth/user-disabled':
+      valorRetorno = 'Não foi possivel logar com sua conta Google';
+      break;
+    default:
+  }
+
+  return valorRetorno;
+};
