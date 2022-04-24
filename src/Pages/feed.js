b414597180146509eac4a607d4a145275e94ef1d
Erika Moreno <erika.kmoreno@gmail.com>
2022-04-24 20:39:21 -0300

    textarea post e btn

--- src/Pages/feed.js
+++ src/Pages/feed.js
@@ -1,22 +1,33 @@
 export default () => {
   const feed = document.createElement('div');
   const templateFeed = `
-  <section class="top-nav">
-  <div>
-    <h1>Logo</h1>
-  </div>
-  <input id="menu-toggle" type="checkbox" />
-  <label class='menu-button-container' for="menu-toggle">
+  <nav class="top-nav">
+    <div>
+      <picture></picture>
+    </div>
+    <input id="menu-toggle" type="checkbox"/>
+      <label class='menu-button-container' for="menu-toggle">
     <div class='menu-button'></div>
-  </label>
-  <ul class="menu">
-    <li>PERFIL</li>
-    <li>PuBLICAR</li>
-    <li>CURTIDAS</li>
-    <li>SAIR</li>
-  </ul>
-</section>`;
-
+      </label>
+    <ul class="menu">
+      <li id="profile">Perfil</li>
+      <li id="logout">Sair</li>
+    </ul>
+  </nav>
+  <section  class="publish "id="publish">
+    <textarea class="post-area-text" placeholder="O que você quer compartilhar?"></textarea>
+    <div id='selected-theme'>
+      <select id='theme'>
+        <option value disabled selected>Assunto</option>
+        <option value="Destinos">Destinos</option>
+        <option value="Dicas">Dicas</option>
+        <option value="Milhas">Milhas</option>
+      </select>
+    </div>
+    <div>
+      <button id="publish-btn">Enviar</button>
+    </div>  
+  </section>`;
   feed.innerHTML = templateFeed;
   return feed;
 };
