5767c0059a78b3f945f1b0476838559fca948961
MariaLuizaSantana <costamalu123@outlook.com>
2022-04-26 17:08:44 -0300

    resolução de conflito função de editar e apagar já funcionando e o css em um processo de estilização

--- src/pages/userprofile/userprofile.js
+++ src/pages/userprofile/userprofile.js
@@ -1,6 +1,5 @@
 import {
-  getUserPosts,
-  getFunctionDelet,
+  getUserPosts
 } from '../../configs/firestore.js';
 import { auth } from '../../configs/authentication.js';
 import header from '../components/header.js';
@@ -31,60 +30,8 @@ export default () => {
 
   printDisplayName.innerHTML = auth.currentUser.displayName;
   printEmail.innerHTML = auth.currentUser.email;
-
-  /*// template do card do post
-  function myPostsCard(text, displayName, date, id = '', likes = []) {
-    const containerMyPost = document.createElement('div');
-    const templateCardMyPost = `
-	  	<div class="card">
-		    <p class="date-card">Postado em:${date}</p}
-		    <section class="post-infos">
-			    <p class="write-message">${text}</p>    
-			    <p class="author">${displayName}</p>
-			    <button class="button-heart">
-				    <i class="fa fa-heart like-btn" id="like-button"></i>
-				    <input type="hidden" id="post-id" value="${id}">
-				    <span class="button-heart-text" id="number-of-likes" data-number="${likes}">${likes.length}</span>
-			    </button>  
-				  <button class="button-edit-post" id="button-edit-post">
-					  <i class="fa fa-pencil"></i>
-				  </button>
-				  <button class="button-delete-post" id="button-delete-post">	
-					  <i class="fa fa-trash"></i>
-				  </button>	
-		    </section>
-	    </div>    
-    `;
-
-    containerMyPost.innerHTML = templateCardMyPost;
-    return containerMyPost;
-  }*/
-
+  
   const myPost = container.querySelector('#my-post');
-
-  const functionDelete = () => {
-    Array.from(myPost).forEach((myPost) => {
-      deleteBtn = myPost.querySelector('#button-delete-post')
-      deleteBtn.addEventListener('click',async()=>{
-        const confirmDeleted = window.confirm("Caro poeta, deseja mesmo deletar esse poema?")
-        if(!confirmDeleted){
-          return;
-        }
-        else{
-          const getIdPost = container.querySelector(myPost.id)
-          await getFunctionDelet(post.id)
-          getIdPost.parentElement.remove()
-          console.log(post.id)
-        }
-      })
-    })
-    //const myTimeline = await getUserPosts(id);
-    //myTimeline.forEach((posts) => {
-      //const getPostId = posts.id
-      //console.log(getPostId)
-   // })
-  }
-  functionDelete()
 
   const showMyPosts = async () => {
     const id = auth.currentUser.uid;
@@ -97,39 +44,6 @@ export default () => {
     });
   };
   showMyPosts();
-
-  //const deleteBtn = container.querySelectorAll("#button-delete-post")
-  //const editBtn = container.querySelectorAll("#button-edit-post")
-
-  console.log(myPost)
-  console.log(typeof(myPost))
-
-  const functionDelete = () => {
-    Array.from(myPost).forEach((post) => {
-      const deleteBtn = container.querySelector('#button-delete-post')
-      deleteBtn.addEventListener('click', async() => {
-        const confirmDeleted = window.confirm("Caro poeta, deseja mesmo deletar esse poema?")
-        if(!confirmDeleted){
-          return;
-        }
-        else{
-          console.log(post.id)
-          const getIdPost = container.getElementById(post.id)
-          await getFunctionDelet(post.id)
-          getIdPost.parentElement.remove()
-          console.log(post.id)
-        }
-      })
-    })
-    //const myTimeline = await getUserPosts(id);
-    //myTimeline.forEach((posts) => {
-      //const getPostId = posts.id
-      //console.log(getPostId)
-   // })
-  }
-  functionDelete()
-
-
-
   return container;
-};
+
+}
