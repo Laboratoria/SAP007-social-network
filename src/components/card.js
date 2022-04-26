23fc9dbb13e931599d57cb90ab6005bf1b881a61
moreirathais <thais_mjs@hotmail.com>
2022-04-26 18:44:50 -0300

    Profile exibindo post do usuário / Validação inputs timeline

--- src/components/card.js
+++ src/components/card.js
@@ -1,22 +1,38 @@
 import { auth } from "../lib/authentication.js";
 import { like } from "../lib/firestore.js";
 
-export default (item, uid) => {
+export default function card(item){
   const publications = document.createElement("div");
-  publications.setAttribute("class", "publicated");
+  publications.setAttribute ("class", "publicated");
   const mold = `
-    <h3 class="published-title">${item.title}</h3>
-    <p class="published-text">${item.text}</p>
-    <div class="user-name">${item.uid}</div>
-    <div>
-      <button class="buttons-like" id="like">like</button>
-      <a href=""class=""edit">editar</a>
-      <a href=""class="delete">deletar</a>
-      <p class="likes"></p>
-    </div>
-    `;
+  <h3 class="published-title">${item.title}</h3>
+  <p class="published-text">${item.text}</p>
+  <div class="user-name">${item.user}</div>
+  <div>
+  <button class="buttons-like" id="like">like</button>
+  <a href=""class=""edit">editar</a>
+  <button class="delete" type="submit" id="delete">deletar<button>
+  <p class="likes"></p>
+  </div>
+  `;
   publications.innerHTML = mold;
-
+  
+  // deleteAction.document.querySelector(".delete").addEventListener("click", (e) => {
+  // e.preventDefault();
+  // console.log("clicou");
+  // // deletePost(docId);
+  // });
+  
+  //const editAction = document.querySelector(".edit");
+  // editAction.addEventListener("click", async(e) => {
+  // e.preventDefault();
+  // edition.innerHTML += `
+  // <textarea class="title" type="text" placeholder="Título"></textarea>
+  // <textarea class="text" type="text" placeholder="Texto" wrap="hard"></textarea>
+  // <button class="btn-post" type="submit">Atualizar</button>
+  // `;
+  // const id = await editPost()
+  // });
   const buttonsLike = publications.querySelector(".buttons-like");
   // const numLikes = publications.querySelector(".likes");
 
@@ -24,6 +40,5 @@ export default (item, uid) => {
     const user = auth.currentUser;
     await like(item.id, user);
   });
-
   return publications;
-};
+  };
