abfcaa5b0568e51c74fa4898756d8ed0bbc0d10c
Louiza Lima <72285598+loulima@users.noreply.github.com>
2022-04-27 15:49:27 -0300

    Update feed.js com as config da agoraVai

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -1,30 +1,39 @@
-import { printNav } from '../components/navbar.js';
-import { postComponent } from '../components/posts.js';
-import { getPosts } from '../lib/firestore.js';
-// import writePost from "../pages/writePost.js";
+//import { getPost } from '../lib/firestore.js';
+//import {postComponent} from '../components/post.js';
+import {printNav} from '../components/navbar.js';
+//import writePost from "../pages/writePost.js";
 
-export default async function feed() {
-  // eslint-disable-next-line no-shadow
-  const feedContainer = document.createElement('section');
-  feedContainer.classList.add('show-posts');
-  feedContainer.id = 'showPosts';
-  // feedContainer.innerHTML = `
-  //  <section class="show-posts" id="showPosts">
-  //  </section>
-  //   `;
-  // feedContainer.appendChild(postComponent(postObj));
-  // feedContainer.appendChild(printNav());
-  feedContainer.innerHTML += printNav();
-  // const sectionPost = feedContainer.querySelector('#showPosts');
+export default function feed() {
+  const feed = document.createElement("section");
+  //const template 
 
-  const showPosts = async () => {
-    const postsArray = await getPosts();
-    postsArray.forEach((postObj) => {
-      const postElement = postComponent(postObj);
-      feedContainer.prepend(postElement);
-    });
-  };
-  showPosts();
-  console.log(feedContainer);
-  return feedContainer;
+  feed.innerHTML= `
+  <section class="show-posts" id="showPosts">
+
+  </section>
+    `;
+
+  feed.appendChild(printNav());
+  //feed.innerHTML+= printNav();
+  
+  
+
+  // const sectionPost = feed.querySelector("#showPosts");
+  // const newRecipe = feed.querySelector("#btn-new-recipe");
+
+  // newRecipe.addEventListener("click", e);
+  // {
+  //   e.preventDefault();
+  //   window.location.hash = "#writePost";
+  // }
+
+  // const showAllPosts = async () => {
+  //   const allPosts = await getPost();
+  //   allPosts.forEach((item) => {
+  //     const postElement = postComponent(item);
+  //     sectionPost.prepend(postElement);
+  //   });
+  // };
+  // showAllPosts();
+  return feed;
 }
