d9d9ead6296404cc9681bb3d34265c6dd7ab3e76
Helena <nannayusuf@gmail.com>
2022-04-27 18:39:02 -0300

    função validar e-mail funcionando

--- src/templates/login.js
+++ src/templates/login.js
@@ -12,8 +12,8 @@ export default function login() {
             <div class="card-login">
                 <h2>LOGIN</h2>
                 <div class="textfield">
-                    <label for="usuario">Usuário</label>
-                    <input type="text" name="usuario" placeholder="Usuário">
+                    <label for="usuario">E-mail</label>
+                    <input type="text" name="e-mail" placeholder="e-mail" id="inputEmail">
                 </div>  
                 <div class="textfield">
                     <label for="senha">Senha</label>
@@ -37,7 +37,26 @@ export default function login() {
 }
 
 
-
+function validacaoEmail(field) {
+  usuario = field.value.substring(0, field.value.indexOf("@"));
+  dominio = field.value.substring(field.value.indexOf("@")+ 1, field.value.length);
+  if ((usuario.length >=1) &&
+      (dominio.length >=3) &&
+      (usuario.search("@")==-1) &&
+      (dominio.search("@")==-1) &&
+      (usuario.search(" ")==-1) &&
+      (dominio.search(" ")==-1) &&
+      (dominio.search(".")!=-1) &&
+      (dominio.indexOf(".") >=1)&&
+      (dominio.lastIndexOf(".") < dominio.length - 1)) {
+  document.getElementById("inputEmail").innerHTML="E-mail válido";
+  alert("email valido");
+  }
+  else{
+  document.getElementById("inputEmail").innerHTML="<font color='red'>Email inválido </font>";
+  alert("E-mail invalido");
+  }
+  }
 
 
 
