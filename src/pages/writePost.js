e3635a856e5224a6c73b0c86ea3497d1ac7a20cc
Louiza Lima <72285598+loulima@users.noreply.github.com>
2022-04-27 15:48:27 -0300

    update writePost com as config da agoraVai

--- src/pages/writePost.js
+++ src/pages/writePost.js
@@ -1,53 +1,28 @@
-// import { signIn} from '../lib/auth.js';
-// import { createPost } from "../lib/firestore.js";
+// import '../configs/config.firebase.js';
+// import { auth } from '../configs/config.firebase.js';
+// import { createPost } from '../lib/firestore.js';
 
 export default function writePost() {
-  const writePostContainer = document.createElement('section');
-  writePostContainer.classList.add('new-post-writePost');
-  const postTemplate = `
+    const writePost = document.createElement('section');
+    writePost.classList.add("new-post-writePost");
+    const postTemplate = `
+    
     <br>
     <textarea id="write-post" class="post-content" placeholder="Postar nova receita" autofocus required	>
     </textarea>
-
     <button class="post-btn">Postar</button>
-
-	  <label for="titulo"> Título</label>
-	  <input type="text" name="titulo" required class="title-input">
-        
-    <label for ="autor"> Autor(a)</label>
-	  <input type="text" name="autor" required class="title-input">
-
-	  <label for ="post">Escreva sua receita</label>
-      <textarea id="write-post" class="post-content" placeholder="Postar nova receita" autofocus required	>
-      </textarea>
-
-      <button class="post-btn">Postar</button>
     <br>
     <button class="return-feed"><a href="#feed"> Voltar para o feed </a></button>
     `;
-  writePostContainer.innerHTML += postTemplate;
+  writePost.innerHTML += postTemplate;
 
-  // const postContent = writePost.querySelector('#write-post');
-  // const btnPost = writePost.querySelector('#post-btn');
-
-  // btnPost.addEventListener("click", (e) => {
-  //     e.preventDefault();
-  //     createPost(postContent.value, auth.currentUser.email);
-  //     window.location.hash = '#feed';
-  // })
-  return writePostContainer;
+    // const postContent = writePost.querySelector('#write-post');
+    // const btnPost = writePost.querySelector('#post-btn');
+    
+    // btnPost.addEventListener("click", (e) => {
+    //     e.preventDefault();
+    //     createPost(postContent.value, auth.currentUser.email);
+    //     window.location.hash = '#feed';
+    // })
+    return writePost
 }
-
-// const titleContent = writePostContainer.querySelector('#title-input');
-// const authorContent = writePostContainer.querySelector('author-input');
-// const recipeContet = writePostContainer.querySelector('#recipe-input');
-//   btnPost.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     if (postContent.value !== " ") {
-//       createPost(postContent.value);
-//       window.location.hash = "#feed";
-//     } else {
-//       alert("deu ruim mané!");
-//     }
-//   });
-// };
