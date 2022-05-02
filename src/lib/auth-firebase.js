f9e318682a9d919a963bf430f3ed940ffc002727
Daiane Gonçalves <daianeecs@icloud.com>
2022-05-02 15:27:23 -0300

    correcao de erro no login e registro

--- src/lib/auth-firebase.js
+++ src/lib/auth-firebase.js
@@ -15,3 +15,4 @@ export function loginGoogle() {
     return credential
     })
 }
+
--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -10,6 +10,7 @@ export default () => {
       <input type="email" id="inputName" placeholder="Insera seu nome completo " requerid /><br>
       <input type="email" id="inputEmail" placeholder="Insera seu email" requerid /><br>
       <input type="password" id="inputSenha" placeholder="Confirme sua senha" requerid /><br>
+      <p id="message" class="successMessage menssage"></p>
     <br><button type="submit" id="btn-Cadastrar">Sign Up</button><br></form>
     `;
 
@@ -19,6 +20,7 @@ const email = containerRegister.querySelector("#inputName");
 const name = containerRegister.querySelector("#inputEmail");
 const password = containerRegister.querySelector("#inputSenha");
 const btnCadastrar = containerRegister.querySelector("#btn-Cadastrar");
+const msgAlert = containerRegister.querySelector('#message');
 const auth = getAuth();
 
 btnCadastrar.addEventListener('click', (e) => {
@@ -26,12 +28,33 @@ btnCadastrar.addEventListener('click', (e) => {
   if (name.value, email.value, password.value) {
     createUserWithEmailAndPassword(auth, name.value, email.value, password.value)
   .then(() => {
-      window.location.hash = "#home";
+     msgAlert.innerHTML = "Registro feito com sucesso!";
+      //window.location.hash = "#home";
   })
   .catch((error) => {
     const errorCode = error.code;
-    if (errorCode === 'auth/account-exists-with-different-credential') {
-      alert("Essa conta já existe com uma credencial diferente");
+    let errorMessage = error.message;
+
+        switch (errorCode) {
+          case 'auth/invalid-email':
+            errorMessage = 'Insira um email válido.';
+            msgAlert.innerHTML = errorMessage;
+            break;
+          case 'auth/weak-password':
+            errorMessage = 'A senha deve ter no mínimo seis caracteres.';
+            msgAlert.innerHTML = errorMessage;
+            break;
+          case 'auth/email-already-in-use':
+            errorMessage = 'Email já cadastrado.';
+            msgAlert.innerHTML = errorMessage;
+            break;
+          case 'auth/missing-email':
+            errorMessage = 'Insira um email.';
+            msgAlert.innerHTML = errorMessage;
+            break;
+          default:
+            errorMessage = 'Preencha todos os campos';
+            msgAlert.innerHTML = errorMessage;
   }
 }) 
 }
