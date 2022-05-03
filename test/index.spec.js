0dc8d20b421c8df426b5dca276b85945dce01bd7
Raynara Pimenta <raynarapimenta@gmail.com>
2022-05-03 14:57:48 -0300

    entrando nos testes

--- test/index.spec.js
+++ test/index.spec.js
@@ -6,9 +6,6 @@ describe('TESTE PARA FUNÇÃO registerNewUser', () => {
   it('testar cadastro de usuario com e-mail e senha de 4 caracteres e espero retornar erro ', () => {
     expect(typeof registerNewUser).toBe('function');
   });
-  it('testar cadastro de usuário com e-mail e senha correta', () => {
-    expect(typeof registerNewUser).toBe('function');
-  });
 });
 
 
