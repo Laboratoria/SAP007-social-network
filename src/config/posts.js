2e127a086c42ddc6e1892e7389f6e44c86076708
Maryssun <maryssol.dayanne@gmail.com>
2022-05-03 13:18:10 -0300

    Modificação na página de posts

--- src/config/posts.js
+++ src/config/posts.js
@@ -2,26 +2,32 @@ import { collection, addDoc } from './export.js';
 import { db, auth } from './start-firebase.js';
 
 export function createNewPost(newText) {
-  if (newText.length === 0) {
-    // colocar mensagem avisando que não tem texto na postagem
-    return;
+  try {
+    if (newText.length === 0) {
+      // colocar mensagem avisando que não tem texto na postagem
+      return;
+    }
+
+    const postsCollection = collection(db, 'posts');
+    const newPost = {
+      userId: auth.currentUser.uid,
+      text: newText,
+      like: 0,
+      createdAt: new Date()
+    };
+
+    addDoc(postsCollection, newPost)
+      .then(() => {
+        window.location.hash = '#feed';
+      })
+      .catch((error) => {
+        return error; // só return ?
+      });
+  } catch (e) {
+    return e;
   }
+}
 
-  const postsCollection = collection(db, 'posts');
-  const newPost = {
-    userId: auth.currentUser.uid,
-    text: newText,
-    like: 0,
-    createdAt: new Date(),
-  };
-
-  addDoc(postsCollection, newPost)
-    .then(() => {
-      window.location.hash = '#feed';
-    })
-    .catch((error) => {
-      return error;
-    });
-
-    //chamar função closeModalAutomatically do arquivo modal.js
+export function getPosts() {
+  // recuperar posts do firebase
 }
