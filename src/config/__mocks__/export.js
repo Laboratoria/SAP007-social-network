4d5dff80beab12242e0e3fcb4b5ec07c290cbd6f
Raynara Pimenta <raynarapimenta@gmail.com>
2022-05-03 13:33:17 -0300

    implementação de um 'pre teste' para começar a executar os testes

--- src/config/__mocks__/export.js
+++ src/config/__mocks__/export.js
@@ -1,4 +1,4 @@
-export const initializeApp = jest.fn();git
+export const initializeApp = jest.fn();
 export const getAuth = jest.fn();
 export const createUserWithEmailAndPassword = jest.fn();
 export const signInWithEmailAndPassword = jest.fn();
--- test/index.spec.js
+++ test/index.spec.js
@@ -3,7 +3,13 @@ import { registerNewUser } from '../src/config/authentication.js';
 jest.mock('../src/config/export.js');
 
 describe('TESTE PARA FUNÇÃO registerNewUser', () => {
-  it('is delicious', () => {
+  it('testar cadastro de usuario com e-mail e senha de 4 caracteres e espero retornar erro ', () => {
+    expect(typeof registerNewUser).toBe('function');
+  });
+  it('testar cadastro de usuário com e-mail e senha correta', () => {
     expect(typeof registerNewUser).toBe('function');
   });
 });
+
+
+// função e fluxo 
