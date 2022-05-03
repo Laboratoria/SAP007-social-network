45aee7c71660c7541eaf9eb660699bc63037a624
moreirathais <thais_mjs@hotmail.com>
2022-05-03 10:55:56 -0300

    mensagem de confirmação deletar

--- src/components/templates.js
+++ src/components/templates.js
@@ -4,22 +4,39 @@ export default function card(item) {
   const publications = document.createElement('div');
   publications.setAttribute('class', 'publicated');
   const mold = `
-    <h3 class="published-title" id="title-${item.id}">${item.title}</h3>
-    <p class="published-text" id="text-${item.id}">${item.text}</p>
-    <div class="user-name">${item.user}</div>
-    <div>
-      <button class="edit">editar</button>
-      <button class="delete" type="submit" id="delete">deletar</button>
-    </div>
-    <span class="edition"></span>
-  `;
+<h3 class="published-title" id="title-${item.id}">${item.title}</h3>
+<p class="published-text" id="text-${item.id}">${item.text}</p>
+<div class="user-name">${item.user}</div>
+<div>
+<button class="edit">editar</button>
+<button class="delete" type="submit" id="delete">deletar</button>
+</div>
+<span class="edition"></span>
+<span class="confirm-delete"></span>
+`;
   publications.innerHTML = mold;
 
   const deleteAction = publications.querySelector('.delete');
+  const deleteConfirm = publications.querySelector('.confirm-delete');
+
   deleteAction.addEventListener('click', async (e) => {
     e.preventDefault();
-    await deletePost(item.id);
-    publications.remove();
+    deleteConfirm.innerHTML += `
+<p>Certeza que deseja deletar a sua contribuição?</p>
+<button class="btn-delete-confirm" id="yes">Sim</button>
+<button class="btn-delete-confirm"id="no">Não</button>
+`;
+    const btnYes = document.getElementById('yes');
+    const btnNo = document.getElementById('no');
+
+    btnYes.addEventListener('click', (e) => {
+      e.preventDefault();
+      deletePost(item.id);
+      publications.remove();
+    });
+    btnNo.addEventListener('click', (e) => {
+      deleteConfirm.innerHTML = '';
+    });
   });
 
   const editAction = publications.querySelector('.edit');
@@ -30,16 +47,16 @@ export default function card(item) {
   editAction.addEventListener('click', (e) => {
     e.preventDefault();
     edition.innerHTML += `
-    <textarea class="title" id="title"type="text" placeholder="Título">${item.title}</textarea>
-    <textarea class="text" id="text"type="text" placeholder="Texto" wrap="hard">${item.text}</textarea>
-    <button class="btn-update" id="update"type="submit">Atualizar</button>
-    `;
+<textarea class="title" id="title"type="text" placeholder="Título">${item.title}</textarea>
+<textarea class="text" id="text"type="text" placeholder="Texto" wrap="hard">${item.text}</textarea>
+<button class="btn-update" id="update"type="submit">Atualizar</button>
+`;
     const valueTitle = document.getElementById('title');
     const valueText = document.getElementById('text');
     const updatePost = document.getElementById('update');
 
-    updatePost.addEventListener('click', (event) => {
-      event.preventDefault();
+    updatePost.addEventListener('click', (e) => {
+      e.preventDefault();
       editPost(item.id, valueTitle.value, valueText.value);
       title.textContent = valueTitle.value;
       text.textContent = valueText.value;
