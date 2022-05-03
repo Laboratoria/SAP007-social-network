a6f9c81ca63aa3f43bb8049e6b801375ba7e4e14
loulima <louizalima13@gmail.com>
2022-05-03 18:05:20 -0300

    botao like nao ta funcionando

--- src/components/posts.js
+++ src/components/posts.js
@@ -1,22 +1,32 @@
+import { auth } from "../configs/config.firebase.js";
+//import { checkLogin } from "../lib/auth.js";
 import { getPosts } from "../lib/firestore.js";
 import { likePost } from "../lib/firestore.js";
 
 export function postComponent(postObj) {
+  // const curtida = postObj.like;
+  const isPostOwner = postObj.userEmail === auth.currentUser.email;
   const postsContainer = document.createElement("div");
   postsContainer.classList.add("new-post-writePost");
 
+ 
   const templatePost = `
     <div class= "post-container">
       <p>${postObj.date}</p>
         <ol class="posts">
           <li> <b>Titulo:${postObj.title}</b>
             <p>Receita por:${postObj.author}</p> 
-            <p>Receita ${postObj.recipe}</p>
+            <p>
+            Receita ${postObj.recipe}
+            </p>
           </li>
         </ol>
       
       <div class='post-interations'>
+
       <button id="cookie-btn"> Curtir </button>
+
+      <p id="num-likes" class="num-likes">Likes: ${postObj.likes.length}</p>
 
       <button id="pencil-btn">Editar</button>
       <button id="trash-btn">Apagar</button> 
@@ -24,40 +34,34 @@ export function postComponent(postObj) {
     </div>
     `;
   const likeButton = postsContainer.querySelector("#cookie-btn");
-  // const editPost = postsContainer.querySelector('#pencil-btn');
-  // const deletePost = postsContainer.querySelector('#trash-btn');
-  // deletePost.forEach((icon) => {
-  //     icon.addEventListener('click', async(event) => {
-  //         event.preventDefault();
-  //         try {
-  //             swal({
-  //                 text:'Tem certeza que quer excluir essa receita?',
-  //             });
-  //             await deletePosts(event.target.dataset.id);
-  //         } catch (error) {
-  //             console.error('error', error);
-  //         }
-  //     });
-  // });
+  const countLikes = postsContainer.querySelector("#num-likes");
+console.log(likeButton);
+
+  //window.onload = function () {
+    //const getUserEmail =checkLogin();
+    likeButton.addEventListener("click", (e) => {
+     console.log('foi!');
+
+      // const postLike = postObj.likes;
+      // if (postLike.includes(auth.currentUser.email)) {
+      //   likePost(postObj.id, auth.currentUser.email).then(() => {
+      //     postLike.push(auth.currentUser.email);
+      //     const addLikeNum = Number(countLikes.innerHTML) + 1;
+      //     countLikes.innerHTML = addLikeNum;
+      //   });
+      // } else {
+      //   dislike(postObj.id, auth.currentUser.email).then(() => {
+      //     postLike.splice(auth.currentUser.email);
+      //     const addLikeNum = Number(countLikes.innerHTML) - 1;
+      //     countLikes.innerHTML = addLikeNum;
+      //   });
+      // }
+     
+    });
+  // };
+
   postsContainer.innerHTML = templatePost;
   getPosts();
-
-  likeButton.addEventListener("click", (e) => {
-    const postLike = post.likes;
-    if (!postLike.includes(getUserEmail)) {
-      like(post.id, getUserEmail).then(() => {
-        postLike.push(getUserEmail);
-        const addLikeNum = Number(countLikes.innerHTML) + 1;
-        countLikes.innerHTML = addLikeNum;
-      });
-    } else {
-      dislike(post.id, getUserEmail).then(() => {
-        postLike.splice(getUserEmail);
-        const addLikeNum = Number(countLikes.innerHTML) - 1;
-        countLikes.innerHTML = addLikeNum;
-      });
-    }
-  });
 
   return postsContainer;
 }
