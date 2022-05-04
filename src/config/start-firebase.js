10eec4e958467b5fb9475751d7de783b98713fd6
Raynara Pimenta <raynarapimenta@gmail.com>
2022-05-04 17:16:30 -0300

    testes de duas funções

--- src/config/start-firebase.js
+++ src/config/start-firebase.js
@@ -1,5 +1,7 @@
 // eslint-disable-next-line
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.9/firebase-app.js';
+import {
+  initializeApp 
+} from 'https://www.gstatic.com/firebasejs/9.6.9/firebase-app.js';
 import { getAuth, getFirestore } from './export.js';
 
 const firebaseConfig = {
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,12 +1,50 @@
-import { registerNewUser } from '../src/config/authentication.js';
+import { registerNewUser, logout } from '../src/config/authentication.js';
+import { loginLabFriends } from '../src/js/pages/login.js' 
 
 jest.mock('../src/config/export.js');
 
-describe('TESTE PARA FUNÇÃO registerNewUser', () => {
-  it('testar cadastro de usuario com e-mail e senha de 4 caracteres e espero retornar erro ', () => {
+describe('Teste de tipo de função', () => {
+  it('registerNewUser é uma função', () => {
     expect(typeof registerNewUser).toBe('function');
+  });
+  it('logout é uma função ', () => {
+    expect(typeof logout).toBe('function');
   });
 });
 
+describe('registerUser', () => {
+  it('Deverá registrar um usuário', () => {
+    registerUser.mockResolvedValueOnce();
+      const name = 'Raynara Pimenta';
+      const email = 'raynarapimenta@gmail.com';
+      const password = '123456';
+      const page = Register();
+      const nameInput = page.querySelector('.name');
+      const emailInput = page.querySelector('.email');
+      const passwordInput = page.querySelector('.password');
+      const btnRegister = page.querySelector('#register-button');
+      nameInput.value = name;
+      emailInput.value = email;
+      passwordInput.value = password;
+      btnRegister.dispatchEvent(new Event('click'));
+    expect(registerUser).toHaveBeenCalledWith(name, email, password);
+    expect(registerUser).toHaveBeenCalledTimes(1);
+  });
+});
 
-// função e fluxo 
+describe('loginLabFriends', () => {
+  it('Deverá logar na timeline', () => {
+    signIn.mockResolvedValueOnce();
+      const email = 'raynarapimenta@gmail.com';
+      const password = '123456';
+      const page = Login();
+      const emailLogin = page.querySelector('.email-login');
+      const passwordLogin = page.querySelector('.password-login');
+      const btnLogin = page.querySelector('#signin-button');
+      emailLogin.value = email;
+      passwordLogin.value = password; 
+      btnLogin.dispatchEvent(new Event('click'));
+    expect(loginLabFriends).toHaveBeenCalledWith(email, password);
+    expect(loginLabFriends).toHaveBeenCalledTimes(1);
+  });
+});
