34de7ad48a8e90c0dc379893431d4091d38879f2
loulima <louizalima13@gmail.com>
2022-05-05 12:20:28 -0300

    falta ajeitar a auth p user editar/apagar post

--- src/components/posts.js
+++ src/components/posts.js
@@ -1,10 +1,12 @@
-import { likePost, dislikePost } from "../lib/firestore.js";
+import { likePost, dislikePost ,} from "../lib/firestore.js";
 import { auth } from "../configs/config.firebase.js";
-import { modalEditPost, modalDeletePost  } from "../components/modal.js";
+import { modalEditPost, modalDeletePost,  } from "../components/modal.js";
+import { getPosts } from "../lib/firestore.js";
 
 export function postComponent(postObj) {
-  // const isPostOwner = postObj.userEmail === auth.currentUser.email;
-  // console.log(isPostOwner);
+  const userId = auth.currentUser.uid;
+  const isAuthor = postObj.user === userId;
+
   const postsContainer = document.createElement("div");
   postsContainer.classList.add("new-post-writePost");
 
@@ -20,37 +22,35 @@ export function postComponent(postObj) {
       
       <div class='post-interations'>
       <button id="cookie-btn"> Curtir </button>
-
       <p class="num-likes">Likes:<span id="num-likes">${postObj.likes.length}</span></p>
-
+      ${isAuthor ? `
       <button id="pencil-btn">Editar</button>
       <button id="trash-btn">Apagar</button> 
-      </div>  
-
       <span id="edit-post"></span>
       <span id="delete-post"></span>
+      ` : ""}
+  </div>  
     </div>
     `;
   postsContainer.innerHTML = templatePost;
+/// MODAIS DE DELETAR E EDITAR
 
-//  if (isPostOwner) {
+ if (isAuthor) {
     const editPost = postsContainer.querySelector("#pencil-btn");
     editPost.addEventListener("click", (e) => {
       e.preventDefault();
       postsContainer.appendChild(modalEditPost(postObj, postsContainer));
     });
-//  }
     const deletePost = postsContainer.querySelector("#trash-btn");
     deletePost.addEventListener("click", (e) => {
       e.preventDefault();
       postsContainer.appendChild(modalDeletePost(postObj, postsContainer));
     });
-
-
-
+ }
+    
+///FUNÇÃO LIKE
   const likeButton = postsContainer.querySelector("#cookie-btn");
   const countLikes = postsContainer.querySelector("#num-likes");
-
 
   likeButton.addEventListener("click", async () => {
     const postLike = postObj.likes;
@@ -72,7 +72,7 @@ export function postComponent(postObj) {
     }
   });
   console.log(likePost(postObj.id, auth.currentUser.email));
-  // getPosts();
+  getPosts();
   return postsContainer;
 }
 
