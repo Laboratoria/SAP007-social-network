ebeef8d334f9e8125a233099f656b6957ffa488d
aline <aline.rosa.cruz@gmail.com>
2022-05-09 16:37:08 -0300

    postagem dando certo

--- src/pages/feed/feed.js
+++ src/pages/feed/feed.js
@@ -1,6 +1,6 @@
-import "../../lib/config-firebase.js";
-//import { lerPost } from "../../lib/config-firestore.js";
-
+import  "../../lib/config-firebase.js";
+import {getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-firestore.js";
+const db = getFirestore();
 
 export default () => { 
     let containerFeed = document.createElement('div');
@@ -8,33 +8,20 @@ export default () => {
     let templateFeed = `
     <p id="post">Post</p>
     <input id="titulo" type="text" placeholder="Título" /><br>
-    <input id="post" type="text" placeholder="Qual a sua teoria?" /><br>
+    <input id="postText" type="text" placeholder="Qual a sua teoria?" /><br>
     <br><button id="btnPost" type="submit" >Postar</button><br>
     `;
 
     containerFeed.innerHTML = templateFeed;
-    console.log("entrou");
-}
-̣ //id='buttonPost
- //const listButtonPost = containerFeed.querySelector("#buttonPost");
 
- 
-    //listButtonPost.addEventListener("click", (e) => {
-      //  e.preventDefault();
-      //  lerPost().then(() => {
-   //     })
-   // });   
-
-    return containerFeed;
-
-
-let inputTitulo = document.getElementById("titulo");
-let inputPost = document.getElementById("post");
-let postBtn = document.getElementById("btnPost");
+let inputTitulo = containerFeed.querySelector("#titulo");
+let inputPost = containerFeed.querySelector("#postText");
+let postBtn = containerFeed.querySelector("#btnPost");
 
 async function addDocument_AutoId() {
     let ref = collection(db, "posts");
-    
+    console.log(inputTitulo.value);
+    console.log(inputPost.value);
     const docRef = await addDoc(
       ref, {
         titulo:inputTitulo.value,
@@ -50,3 +37,6 @@ async function addDocument_AutoId() {
 }
 
 postBtn.addEventListener("click", addDocument_AutoId);
+
+return containerFeed;
+}
