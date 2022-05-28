e3653c28dd62f7b08cec09ee646f2f2cc86b3f50
aline <aline.rosa.cruz@gmail.com>
2022-05-28 10:54:05 -0300

    chore: correcao login do google

--- src/lib/config-firebase.js
+++ src/lib/config-firebase.js
@@ -2,6 +2,7 @@ import { initializeApp } from './exports.js';
 
 const firebaseConfig = {
   apiKey: "AIzaSyB1PM6Kq8qggIFKLEd8zqXex_WTnde7sCw",
+  authDomain: "alem-91779.firebaseapp.com",
   projectId: "alem-91779",
   storageBucket: "alem-91779.appspot.com",
   messagingSenderId: "1076428810136",
